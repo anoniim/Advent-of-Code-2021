1c0a692c7df60f6b2e77645696807907b1e026e4
Marcel Broz <marcel.broz@callsign.com>
2021-12-04 21:01:18 +0000

    Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,28 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        var increaseCount = 0
+        input.windowed(2)
+            .forEach {
+                if (it[0] < it[1]) increaseCount++
+            }
+        return increaseCount
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        var increaseCount = 0
+        input.windowed(3, partialWindows = false) { it.sum() }
+            .windowed(2)
+            .forEach {
+                if (it[0] < it[1]) increaseCount++
+            }
+        return increaseCount
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInts("Day01_test")
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInts("Day01")
     println(part1(input))
     println(part2(input))
 }
