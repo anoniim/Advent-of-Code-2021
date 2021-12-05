ce4a9cc2eda0be28e7a79d0093e33126465f964c
Marcel Broz <marcel.broz@gmail.com>
2021-12-05 12:57:56 +0000

    Day 5 part 2 solution

--- src/Day05.kt
+++ src/Day05.kt
@@ -8,8 +8,8 @@ private class Day05 {
         fun part1(input: List<Pair<Point, Point>>): Int {
             // Find the most dangerous areas
             val map = mutableMapOf<Point, Int>()
-            input.filter(this::onlyHorizontalAndVertical)
-                .map(this::toListOfPoints)
+            input.filter(this::isHorizontalOrVertical)
+                .map(this::lineToListOfPoints)
                 .flatten()
                 .forEach { point ->
                     val count = map[point] ?: 0
@@ -19,27 +19,40 @@ private class Day05 {
         }
 
         fun part2(input: List<Pair<Point, Point>>): Int {
-            return 0
+            // Find the most dangerous areas including diagonals
+            val map = mutableMapOf<Point, Int>()
+            input.map(this::lineToListOfPoints)
+                .flatten()
+                .forEach { point ->
+                    val count = map[point] ?: 0
+                    map[point] = count + 1
+                }
+            return map.count { it.value >= 2 }
         }
 
         // test if implementation meets criteria from the description, like:
         val testInput = parseInput("${this::class.simpleName}_test")
         check(part1(testInput) == 5)
-        check(part2(testInput) == 0)
+        check(part2(testInput) == 12)
 
         val input = parseInput("${this::class.simpleName}")
         println(part1(input))
         println(part2(input))
     }
 
-    private fun onlyHorizontalAndVertical(pair: Pair<Point, Point>): Boolean {
+    private fun isHorizontalOrVertical(pair: Pair<Point, Point>): Boolean {
         return pair.first.x == pair.second.x || pair.first.y == pair.second.y
     }
 
-    private fun toListOfPoints(pair: Pair<Point, Point>): List<Point> {
+    private fun lineToListOfPoints(pair: Pair<Point, Point>): List<Point> {
         val xRange = IntProgression.fromClosedRange(pair.first.x, pair.second.x, getStep(pair.first.x, pair.second.x))
         val yRange = IntProgression.fromClosedRange(pair.first.y, pair.second.y, getStep(pair.first.y, pair.second.y))
-        return xRange.flatMap { x -> yRange.map { y -> Point(x, y) } }
+        return if(isHorizontalOrVertical(pair)) {
+            xRange.flatMap { x -> yRange.map { y -> Point(x, y) } }
+        } else {
+            // Diagonal
+            xRange.zip(yRange) { x, y -> Point(x, y) }
+        }
     }
 
     private fun getStep(first: Int, second: Int): Int {
