b19d737d3f2f5029e385bccff5cbedb321f9ce08
Marcel Broz <marcel.broz@gmail.com>
2021-12-16 22:28:40 +0000

    Template for solution classes

---
+++ src/DayTemplate.kt
@@ -0,0 +1,27 @@
+fun main() = Day().main()
+
+private class Day {
+
+    fun main() {
+        // test if implementation meets criteria from the description:
+        val testInput = parseInput("${this::class.simpleName}_test")
+        check(part1(testInput) == 0)
+        check(part2(testInput) == 0)
+
+        val input = parseInput("${this::class.simpleName}")
+        println(part1(input))
+        println(part2(input))
+    }
+
+    fun part1(input: List<Int>): Int {
+        //
+        return 0
+    }
+
+    fun part2(input: List<Int>): Int {
+        //
+        return 0
+    }
+
+    private fun parseInput(inputFile: String) = readInput(inputFile)[0].split(',').map { it.toInt() }
+}
