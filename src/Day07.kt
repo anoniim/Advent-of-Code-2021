4eb25495fe8e9520e5f42f76f0a6a69e6daff7c1
Marcel Broz <marcel.broz@gmail.com>
2021-12-16 23:16:51 +0000

    Day 7 part 2 solution

--- src/Day07.kt
+++ src/Day07.kt
@@ -8,7 +8,7 @@ private class Day07 {
         // test if implementation meets criteria from the description:
         val testInput = parseInput("${this::class.simpleName}_test")
         check(part1(testInput) == 37)
-        check(part2(testInput) == 0)
+        check(part2(testInput) == 168)
 
         val input = parseInput("${this::class.simpleName}")
         println(part1(input))
@@ -37,7 +37,32 @@ private class Day07 {
     }
 
     fun part2(input: List<Int>): Int {
-        return 0
+        // Updated cost algorithm
+        val minPosition = input.minOf { it }
+        val maxPosition = input.maxOf { it }
+        var minCost = Int.MAX_VALUE
+        for (position in minPosition..maxPosition) {
+            val cost = calculateCostPart2(position, input)
+            if (cost < minCost) minCost = cost
+        }
+        return minCost
+    }
+
+    private fun calculateCostPart2(target: Int, input: List<Int>): Int {
+        var cost = 0
+        for (position in input) {
+            val distance = abs(target - position)
+            cost += calculateCostForDistance(distance)
+        }
+        return cost
+    }
+
+    private fun calculateCostForDistance(distance: Int): Int {
+        var cost = 0
+        for (i in distance downTo 0) {
+            cost += i
+        }
+        return cost
     }
 
     private fun parseInput(inputFile: String) = readInput(inputFile)[0].split(',').map { it.toInt() }
