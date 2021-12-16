dff1879f527fd21a3cb9f307bcdb7ba4f4cb2980
Marcel Broz <marcel.broz@gmail.com>
2021-12-16 23:16:27 +0000

    Day 7 part 1 solution

---
+++ src/Day07.kt
@@ -0,0 +1,44 @@
+import kotlin.math.abs
+
+fun main() = Day07().main()
+
+private class Day07 {
+
+    fun main() {
+        // test if implementation meets criteria from the description:
+        val testInput = parseInput("${this::class.simpleName}_test")
+        check(part1(testInput) == 37)
+        check(part2(testInput) == 0)
+
+        val input = parseInput("${this::class.simpleName}")
+        println(part1(input))
+        println(part2(input))
+    }
+
+    fun part1(input: List<Int>): Int {
+        // What's the cheapest position and how much fuel do crabs spend to align to that position?
+        // TODO Order and find median, then test values from there
+        val minPosition = input.minOf { it }
+        val maxPosition = input.maxOf { it }
+        var minCost = Int.MAX_VALUE
+        for (position in minPosition..maxPosition) {
+            val cost = calculateCostPart1(position, input)
+            if (cost < minCost) minCost = cost
+        }
+        return minCost
+    }
+
+    private fun calculateCostPart1(target: Int, input: List<Int>): Int {
+        var cost = 0
+        for (position in input) {
+            cost += abs(target - position)
+        }
+        return cost
+    }
+
+    fun part2(input: List<Int>): Int {
+        return 0
+    }
+
+    private fun parseInput(inputFile: String) = readInput(inputFile)[0].split(',').map { it.toInt() }
+}
