032344a05c1d90e63b505e554ee144e4f7ad42f0
Marcel Broz <marcel.broz@gmail.com>
2021-12-17 21:49:32 +0000

    Day 6 part 2 solution with multi-threading (it didn't help)

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,3 +1,9 @@
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import kotlin.system.measureTimeMillis
+
 fun main() {
     Day06().main()
 }
@@ -8,11 +14,17 @@ private class Day06 {
         // test if implementation meets criteria from the description:
         val testInput = parseInput("${this::class.simpleName}_test")
         check(part1(testInput) == 5934)
-        check(part2(testInput) == 26984457539)
+        measureTimeMillis {
+            check(part2(testInput) == 26984457539)
+        }.also { time -> println("Completed in ${time/1000}s") }
+
+        measureTimeMillis {
+            check(part2MultiThreaded(testInput) == 26984457539)
+        }.also { time -> println("Completed in ${time/1000}s") }
 
         val input = parseInput("${this::class.simpleName}")
         println(part1(input))
-//        println(part2(input))
+//        println(part2(input)) // FIXME takes way too long
     }
 
     fun part1(input: List<Int>): Int {
@@ -33,18 +45,36 @@ private class Day06 {
 
     fun part2(input: List<Int>): Long {
         // How many lanternfish are there after 256 days?
-        // TODO implement coroutines to calculate sum for each initial fish in parallel
         return input.sumOf { sumAllInGeneration(0, it+1, 256) }
     }
 
     fun sumAllInGeneration(day: Int, timeToNextGen: Int, maxDay: Int): Long {
         val nexGenDay = day + timeToNextGen
-        if (nexGenDay > maxDay) return 1
+        if (nexGenDay > maxDay) return 1L
         var sumOfAllChildren = 0L
         for(birthDay in nexGenDay .. maxDay step 7) {
             sumOfAllChildren += sumAllInGeneration(birthDay, 9, maxDay)
         }
         return 1L + sumOfAllChildren
+    }
+
+    fun part2MultiThreaded(input: List<Int>): Long {
+        // How many lanternfish are there after 256 days?
+        return runBlocking {
+            input.map {
+                async { sumAllInGenerationSuspended(0, it+1, 256) }
+            }.sumOf { it.await() }
+        }
+    }
+
+    suspend fun sumAllInGenerationSuspended(day: Int, timeToNextGen: Int, maxDay: Int): Long = withContext(Dispatchers.Default) {
+        val nexGenDay = day + timeToNextGen
+        if (nexGenDay > maxDay) return@withContext 1L
+        var sumOfAllChildren = 0L
+        for(birthDay in nexGenDay .. maxDay step 7) {
+            sumOfAllChildren += sumAllInGenerationSuspended(birthDay, 9, maxDay)
+        }
+        1L + sumOfAllChildren
     }
 
     private fun parseInput(inputFile: String) = readInput(inputFile)[0].split(',').map { it.toInt() }
